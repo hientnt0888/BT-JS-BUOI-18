40f4a58e7ba71735dc7395b2fbb6bd9c7e9d2654
Hien <hientnt0999@gmail.com>
2023-08-08 23:19:52 +0700

    update

--- js/SSTang.js
+++ js/SSTang.js
@@ -1,8 +1,12 @@
 function timSoChanCC() {
-    arrayList.sort(function(a,b){
-        return a - b;
-    })
-    document.querySelector("#sumSSepTang").innerHTML = "👉 Danh sách tăng dần: " + arrayList;
+    var arrayListCopy = [];
+    for (var i = 0; i< arrayList.length; i++ ){
+        arrayListCopy.push(arrayList[i]);
+    }
+        arrayListCopy.sort(function (a, b) {
+            return a - b;
+        })
+    document.querySelector("#sumSSepTang").innerHTML = "👉 Danh sách tăng dần: " + arrayListCopy;
 
 }
 document.querySelector("#btnSSepTang").onclick = timSoChanCC;
